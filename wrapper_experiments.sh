888931d2091256b39e6775acaab85efeb7f7edc3
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2020-04-16 16:33:31 +0200

    Update wrapper_experiments.sh

--- wrapper_experiments.sh
+++ wrapper_experiments.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-python3 experiments_launcher.py -p auto -r results
-python3 experiments_launcher.py -p quemy -r results
-python3 experiments_launcher.py -p pseudo-exhaustive -r results
-python3 results_processors/auto_quemy_comparator.py -ia ../results/auto -iq ../results/quemy -ip ../results/pseudo-exhaustive -o ../results
+python3 experiments_launcher.py -p pseudo-exhaustive -r results/pipeline_algorithm
