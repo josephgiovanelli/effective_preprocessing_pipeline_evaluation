5e8dcf377e99df7cc4d9a486173bc22785fca777
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2020-05-16 13:19:09 +0200

    Update results_extraction_utils.py

--- results_processors/results_extraction_utils.py
+++ results_processors/results_extraction_utils.py
@@ -1,13 +1,17 @@
 import collections
 import os
 import json
+import string
 
 import pandas as pd
 
 from os import listdir
 from os.path import isfile, join
 
+import matplotlib.pyplot as plt
+
 from commons import benchmark_suite, algorithms
+
 
 def get_filtered_datasets():
     df = pd.read_csv('meta_features/simple-meta-features.csv')
@@ -118,11 +122,47 @@ def summarize_winners(winners_map):
 def save_summary(summary_map, result_path):
     if os.path.exists('summary.csv'):
         os.remove('summary.csv')
+    total = {}
     for algorithm, value in summary_map.items():
+        pipelines = []
+        winners = []
         with open(os.path.join(result_path, '{}.csv'.format(algorithm)), 'w') as out:
             out.write('pipeline,winners\n')
-            for pipeline, winners in value.items():
-                out.write(str(pipeline) + ',' + str(winners) + '\n')
+            for pipeline, winner in value.items():
+                out.write(str(pipeline) + ',' + str(winner) + '\n')
+                pipelines.append(str(pipeline))
+                winners.append(int(winner))
+                total[str(pipeline)] = winner if not(str(pipeline) in total.keys()) else total[str(pipeline)] + winner
+
+        plt.rcdefaults()
+        plt.bar(pipelines, winners)
+
+        plt.xlabel('Prototype IDs')
+        plt.ylabel('Number of data-sets for which a prototype\nachieved the best performance')
+        plt.ylim(0, 10)
+        plt.title('Comparison of the goodness of the prototypes for {}'.format(algorithm.upper()))
+
+        fig = plt.gcf()
+        fig.set_size_inches(10, 5, forward=True)
+        fig.savefig(os.path.join(result_path, '{}.pdf'.format(algorithm)))
+
+        plt.clf()
+
+    plt.rcdefaults()
+    plt.bar(total.keys(), total.values())
+
+    plt.xlabel('Prototype IDs')
+    plt.ylabel('Number of times for which a prototype\nachieved the best performance')
+    plt.ylim(0, 20)
+    plt.title('Comparison of the goodness of the prototypes (all data-sets and algorithms together)')
+
+    fig = plt.gcf()
+    fig.set_size_inches(10, 5, forward=True)
+    fig.savefig(os.path.join(result_path, 'summary.pdf'))
+
+    plt.clf()
+
+
 
 def save_comparison(results_pipelines, results_auto, result_path):
     if os.path.exists('comparison.csv'):
