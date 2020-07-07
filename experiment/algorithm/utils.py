4e818f7ebdf0d8833ab40a532dc5e3bcdc79fa8a
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2020-07-07 21:56:48 +0200

    Clean master

--- experiment/algorithm/utils.py
+++
@@ -1,7 +0,0 @@
-from hyperopt import hp
-
-def generate_domain_space(prototype):
-    domain_space = {}
-    for k, v in prototype.items():
-        domain_space[k] = hp.choice(k, v)
-    return domain_space
