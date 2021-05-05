dbbb4d2f3439612a0542266fbf3077c22a5fc184
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2021-05-05 10:02:44 +0200

    Update meta_learning_input_preparation.py

--- results_processors/meta_learning_input_preparation.py
+++ results_processors/meta_learning_input_preparation.py
@@ -8,6 +8,8 @@ import itertools
 import copy
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 from utils import create_directory
 
@@ -28,56 +30,84 @@ def main():
     study_1 = pd.read_csv('meta_features/extended_meta_features_study_1.csv')
     all_classification = all_classification[all_classification['ID'].isin(diff(all_classification['ID'], openml_cc_18['ID']))]
     meta_features = pd.concat([openml_cc_18, all_classification, study_1], ignore_index=True, sort=True)
-    
+
+    baseline_results = {}
     ## Results Loading
-    df = {}
     for algorithm in algorithms:
-        df[algorithm] = pd.read_csv(os.path.join(input, algorithm + '.csv'))
-        df[algorithm].rename(columns={"dataset": "ID"}, inplace=True)
+        baseline_results[algorithm] = pd.read_csv(os.path.join(input, algorithm + '.csv'))
+        baseline_results[algorithm].rename(columns={"dataset": "ID"}, inplace=True)
+
+    results_map = {}
+    for algorithm in algorithms:
+        results_map[algorithm] = pd.DataFrame()
+
+        df = pd.read_csv(os.path.join("..", "results", "summary", "evaluation3", algorithm + ".csv"))
+        ids = list(df["dataset"])
+
+        files = [f for f in listdir(input_auto) if isfile(join(input_auto, f))]
+        results = [f[:-5] for f in files if f[-4:] == 'json']
+
+        for dataset in ids:
+            acronym = algorithm + "_" + str(dataset)
+            if acronym in results:
+                with open(os.path.join(input_auto, acronym + '.json')) as json_file:
+                    data = json.load(json_file)
+                    pipeline = data['context']['best_config']['pipeline']
+
+                    encode_flag = 1 if "None" not in pipeline["encode"][0] else 0
+                    features_flag = 1 if "None" not in pipeline["features"][0] else 0
+                    impute_flag = 1 if "None" not in pipeline["impute"][0] else 0
+                    try:
+                        normalize_flag = 1 if "None" not in pipeline["normalize"][0] else 0
+                    except:
+                        normalize_flag = 0
+                    try:
+                        discretize_flag = 1 if "None" not in pipeline["discretize"][0] else 0
+                    except:
+                        discretize_flag = 0
+                    rebalance_flag = 1 if "None" not in pipeline["rebalance"][0] else 0
+
+                    results_map[algorithm] = results_map[algorithm].append(pd.DataFrame({
+                        "ID": [dataset], 
+                        "baseline": [baseline_results[algorithm].loc[baseline_results[algorithm]["ID"] == dataset, "baseline"].iloc[0]],
+                        "encode": [encode_flag],
+                        "features": [features_flag],
+                        "impute": [impute_flag],
+                        "normalize": [normalize_flag],
+                        "discretize": [discretize_flag],
+                        "rebalance": [rebalance_flag]
+                        }), ignore_index=True)
+
+    for algorithm in algorithms:
+        results_map[algorithm] = pd.merge(results_map[algorithm], meta_features, on="ID")
+
+    ## Data Saving
+    meta_learning_input_path = create_directory(result_path, "simpler_meta_learning")
+    meta_learning_input_path = create_directory(meta_learning_input_path, "input")
+    for algorithm in algorithms:
+        results_map[algorithm].to_csv(os.path.join(meta_learning_input_path, algorithm + '_raw.csv'), index=False)
+
+    data = copy.deepcopy(results_map)
+
+    ## Data Preparation
+    benchmarking_features = ['CfsSubsetEval_NaiveBayesErrRate', 'NaiveBayesErrRate', 'RandomTreeDepth2AUC', 'RandomTreeDepth3Kappa', 'REPTreeDepth2AUC', 'J48.001.ErrRate', 'CfsSubsetEval_DecisionStumpKappa', 'J48.0001.ErrRate', 'J48.00001.Kappa', 'RandomTreeDepth2ErrRate', 'REPTreeDepth2ErrRate', 'CfsSubsetEval_NaiveBayesKappa', 'RandomTreeDepth2Kappa', 'RandomTreeDepth3ErrRate', 'REPTreeDepth1AUC', 'REPTreeDepth1ErrRate', 'REPTreeDepth2Kappa', 'NaiveBayesKappa', 'CfsSubsetEval_NaiveBayesAUC', 'REPTreeDepth3Kappa', 'kNN1NAUC', 'J48.001.Kappa', 'DecisionStumpErrRate', 'DecisionStumpAUC', 'J48.00001.AUC', 'REPTreeDepth1Kappa', 'RandomTreeDepth3AUC', 'REPTreeDepth3AUC', 'J48.0001.Kappa', 'CfsSubsetEval_DecisionStumpErrRate', 'J48.00001.ErrRate', 'RandomTreeDepth1Kappa', 'RandomTreeDepth1AUC', 'CfsSubsetEval_kNN1NErrRate', 'DecisionStumpKappa', 'kNN1NErrRate', 'kNN1NKappa', 'J48.001.AUC', 'CfsSubsetEval_kNN1NAUC', 'NaiveBayesAUC', 'CfsSubsetEval_DecisionStumpAUC', 'RandomTreeDepth1ErrRate', 'CfsSubsetEval_kNN1NKappa', 'REPTreeDepth3ErrRate', 'J48.0001.AUC']
+    manual_fs_data = data.copy()
+    for algorithm in algorithms:
+        manual_fs_data[algorithm].drop(benchmarking_features, axis=1, inplace=True)
+
+    ## Data Saving
+    for algorithm in algorithms:
+        manual_fs_data[algorithm].to_csv(os.path.join(meta_learning_input_path, 'manual_fs_' + algorithm + '.csv'), index=False)
 
     ## Data Preparation
     for algorithm in algorithms:
-        df[algorithm] = pd.merge(df[algorithm], meta_features, on="ID")
+        data[algorithm]["algorithm"] = algorithm
+        manual_fs_data[algorithm]["algorithm"] = algorithm
+    union = pd.concat([data["knn"], data["nb"], data["rf"]], ignore_index=True)
+    manual_fs_union = pd.concat([manual_fs_data["knn"], manual_fs_data["nb"], manual_fs_data["rf"]], ignore_index=True)
 
     ## Data Saving
-    meta_learning_input_path = create_directory(result_path, "meta_learning")
-    meta_learning_input_path = create_directory(meta_learning_input_path, "input")
-    for algorithm in algorithms:
-        df[algorithm].to_csv(os.path.join(meta_learning_input_path, algorithm + '_raw.csv'), index=False)
-
-    for pa_impact in [True, False]:
-        data = copy.deepcopy(df)
-
-        ## Data Preparation
-        for algorithm in algorithms:
-            if pa_impact:
-                data[algorithm]["pa_impact"] = data[algorithm]["pipeline_algorithm"] - data[algorithm]["baseline"]
-                data[algorithm].drop(['pa_percentage'], axis=1, inplace=True)
-            data[algorithm].drop(['pipeline_algorithm', 'algorithm', 'a_score', 'pa_score', 'a_percentage'], axis=1, inplace=True)
-
-        ## Data Saving
-        for algorithm in algorithms:
-            data[algorithm].to_csv(os.path.join(meta_learning_input_path, algorithm + ('_pa_impact' if pa_impact else '') + '.csv'), index=False)
-
-        ## Data Preparation
-        benchmarking_features = ['CfsSubsetEval_NaiveBayesErrRate', 'NaiveBayesErrRate', 'RandomTreeDepth2AUC', 'RandomTreeDepth3Kappa', 'REPTreeDepth2AUC', 'J48.001.ErrRate', 'CfsSubsetEval_DecisionStumpKappa', 'J48.0001.ErrRate', 'J48.00001.Kappa', 'RandomTreeDepth2ErrRate', 'REPTreeDepth2ErrRate', 'CfsSubsetEval_NaiveBayesKappa', 'RandomTreeDepth2Kappa', 'RandomTreeDepth3ErrRate', 'REPTreeDepth1AUC', 'REPTreeDepth1ErrRate', 'REPTreeDepth2Kappa', 'NaiveBayesKappa', 'CfsSubsetEval_NaiveBayesAUC', 'REPTreeDepth3Kappa', 'kNN1NAUC', 'J48.001.Kappa', 'DecisionStumpErrRate', 'DecisionStumpAUC', 'J48.00001.AUC', 'REPTreeDepth1Kappa', 'RandomTreeDepth3AUC', 'REPTreeDepth3AUC', 'J48.0001.Kappa', 'CfsSubsetEval_DecisionStumpErrRate', 'J48.00001.ErrRate', 'RandomTreeDepth1Kappa', 'RandomTreeDepth1AUC', 'CfsSubsetEval_kNN1NErrRate', 'DecisionStumpKappa', 'kNN1NErrRate', 'kNN1NKappa', 'J48.001.AUC', 'CfsSubsetEval_kNN1NAUC', 'NaiveBayesAUC', 'CfsSubsetEval_DecisionStumpAUC', 'RandomTreeDepth1ErrRate', 'CfsSubsetEval_kNN1NKappa', 'REPTreeDepth3ErrRate', 'J48.0001.AUC']
-        manual_fs_data = data.copy()
-        for algorithm in algorithms:
-            manual_fs_data[algorithm].drop(benchmarking_features, axis=1, inplace=True)
-
-        ## Data Saving
-        for algorithm in algorithms:
-            manual_fs_data[algorithm].to_csv(os.path.join(meta_learning_input_path, 'manual_fs_' + algorithm + ('_pa_impact' if pa_impact else '') + '.csv'), index=False)
-
-        ## Data Preparation
-        for algorithm in algorithms:
-            data[algorithm]["algorithm"] = algorithm
-            manual_fs_data[algorithm]["algorithm"] = algorithm
-        union = pd.concat([data["knn"], data["nb"], data["rf"]], ignore_index=True)
-        manual_fs_union = pd.concat([manual_fs_data["knn"], manual_fs_data["nb"], manual_fs_data["rf"]], ignore_index=True)
-
-        ## Data Saving
-        union.to_csv(os.path.join(meta_learning_input_path, 'union' + ('_pa_impact' if pa_impact else '') + '.csv'), index=False)
-        manual_fs_union.to_csv(os.path.join(meta_learning_input_path, 'manual_fs_union' + ('_pa_impact' if pa_impact else '') + '.csv'), index=False)
+    union.to_csv(os.path.join(meta_learning_input_path, 'union' + '.csv'), index=False)
+    manual_fs_union.to_csv(os.path.join(meta_learning_input_path, 'manual_fs_union' + '.csv'), index=False)
     
 main()
