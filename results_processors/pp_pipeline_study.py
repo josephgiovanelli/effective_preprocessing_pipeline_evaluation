7d9c05bb7e7df9eb46b95e90bfe61d33fdbb4bb2
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2021-06-18 18:00:40 +0200

    Made barplots nicer

--- results_processors/pp_pipeline_study.py
+++ results_processors/pp_pipeline_study.py
@@ -7,6 +7,8 @@ import json
 import itertools
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 from utils import create_directory
 
@@ -16,9 +18,10 @@ def main():
     input_auto = "../results/evaluation3/preprocessing_algorithm"
     result_path = create_directory(result_path, "extension")
 
+    discretize_count, normalize_count = {}, {}
     results_map = pd.DataFrame()
-    for algorithm in ["knn", "nb", "rf"]:
-
+    for algorithm in ["nb", "knn", "rf"]:
+        discretize_count[algorithm], normalize_count[algorithm] = 0, 0
         df = pd.read_csv(os.path.join("..", "results", "summary", "evaluation3", algorithm + ".csv"))
         #df = df[(df["pa_percentage"] == 0.5)]
         ids = list(df["dataset"])
@@ -39,10 +42,12 @@ def main():
                     impute_flag = 1 if "None" not in pipeline["impute"][0] else 0
                     try:
                         normalize_flag = 1 if "None" not in pipeline["normalize"][0] else 0
+                        normalize_count[algorithm] += 1
                     except:
                         normalize_flag = 0
                     try:
                         discretize_flag = 1 if "None" not in pipeline["discretize"][0] else 0
+                        discretize_count[algorithm] += 1
                     except:
                         discretize_flag = 0
                     rebalance_flag = 1 if "None" not in pipeline["rebalance"][0] else 0
@@ -50,20 +55,76 @@ def main():
                     results_map = results_map.append(pd.DataFrame({
                         "algorithm": [algorithm],
                         "dataset": [dataset],
-                        "encode": [encode_flag],
-                        "features": [features_flag],
-                        "impute": [impute_flag],
-                        "normalize": [normalize_flag],
-                        "discretize": [discretize_flag],
-                        "rebalance": [rebalance_flag]
+                        "E": [encode_flag],
+                        "N": [normalize_flag],
+                        "D": [discretize_flag],
+                        "I": [impute_flag],
+                        "R": [rebalance_flag],
+                        "F": [features_flag]
                     }), ignore_index=True)
 
-        results_map.to_csv(os.path.join(result_path, "pp_pipeline_study.csv"), index=False)
+    results_map.to_csv(os.path.join(result_path, "pp_pipeline_study.csv"), index=False)
 
-        result = results_map.groupby(['algorithm']).sum()
-        result = result.reset_index()
-        result = result.drop(['dataset'], axis=1)
-        result.to_csv(os.path.join(result_path, "pp_pipeline_study_grouped.csv"), index=False)
+    result = results_map.groupby(['algorithm']).sum()
+    result = result.reset_index()
+    result = result.drop(['dataset'], axis=1)
+    result = result.set_index(["algorithm"])
+    result = result.reindex(["nb", "knn", "rf"])
 
+    for algorithm in ["nb", "knn", "rf"]:
+        result.loc[algorithm,'N'] /= normalize_count[algorithm]
+        result.loc[algorithm,'D'] /= discretize_count[algorithm]
+    result = result.reset_index()
+    result['E'] = result['E'] / 80
+    result['I'] = result['I'] / 80
+    result['R'] = result['R'] / 80
+    result['F'] = result['F'] / 80
+    print(normalize_count, discretize_count)
+    result.to_csv(os.path.join(result_path, "pp_pipeline_study_grouped.csv"), index=False)
+    result = result.rename(columns={
+        'E' : r'$E$',
+        'I' : r'$I$',
+        'N' : r'$N$',
+        'D' : r'$D$',
+        'F' : r'$F$',
+        'R' : r'$R$',
+    })
+    labels = [x.upper() for x in result["algorithm"]]
+    colors = ['mediumpurple', 'greenyellow', 'lightcoral', 'gold', 'mediumturquoise', 'orange']
+    x = np.arange(len(labels))  # the label locations
+    width = 0.125  # the width of the bars
+    SMALL_SIZE = 8
+    MEDIUM_SIZE = 15
+    BIGGER_SIZE = 21
+
+    plt.rc('font', size=MEDIUM_SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=MEDIUM_SIZE)  # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=MEDIUM_SIZE)  # legend fontsize
+    plt.rc('figure', titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+    fig, ax = plt.subplots()
+    rects = {}
+    columns = list(result.columns)[1:]
+    for column in range(len(columns)):
+        ax.bar((x * width * 8) + (width * (column - 1)) - 0.2, result[columns[column]], width, color=colors[column], label=columns[column])
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Frequency')
+    
+    vals = ax.get_yticks()
+    ax.set_yticklabels(['{:,.0%}'.format(x) for x in vals])
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    #ax.legend()
+
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    lgd = fig.legend(by_label.values(), by_label.keys(), loc='lower center', ncol = 8, bbox_to_anchor=(0.55, 1.0))
+    text = fig.text(-0.2, 1.05, "", transform=ax.transAxes)
+    fig.tight_layout()
+    fig.set_size_inches(10, 5)
+    fig.savefig(os.path.join(result_path, "pp_pipeline_study_grouped.pdf"), bbox_extra_artists=(lgd,text), bbox_inches='tight')
 
 main()
--- results_processors/pp_pipeline_study2.py
+++ results_processors/pp_pipeline_study2.py
@@ -74,10 +74,70 @@ def main():
     results_map = results_map.pivot_table(index='algorithm', columns='pipeline', aggfunc=len, fill_value=0)
     results_map["sum"] = results_map.sum(axis=1)
     results_map = results_map.div(results_map["sum"], axis=0)
-    results_map = results_map.drop(['sum'], axis=1)
+    results_map = results_map.drop(['sum'], axis=1) 
+    results_map = results_map.reset_index()
+    results_map = results_map.set_index(["algorithm"])
+    results_map = results_map.reindex(["nb", "knn", "rf"])
     results_map = results_map.reset_index()
     results_map.to_csv(os.path.join(result_path, "pp_pipeline_study2_pivoted.csv"), index=False)
+    results_map = results_map.rename(columns={
+        'DF': r'$D \to F$',
+        'RF': r'$R \to F$',
+        'D': r'$D$',
+        'N': r'$N$',
+        'R': r'$R$',
+        'DFR': r'$D \to F \to R$',
+        'DR': r'$D \to R$',
+        'DRF': r'$D \to R \to F$',
+        'NFR': r'$N \to F \to R$',
+        'NRF': r'$N \to R \to F$',
+        'RD': r'$R \to D$',
+        'DF': r'$D \to F$',
+        'DR': r'$D \to R$',
+        'FR': r'$F \to R$',
+        'NF': r'$N \to F$',
+        'NR': r'$N \to R$',
+        'RDF': r'$R \to D \to F$',
+    })
+    print(results_map)
+    labels = [x.upper() for x in results_map["algorithm"]]
+    patterns = ['/', '\\', '-\\', '-', '+', 'x', 'o', '//', '\\\\', 'O.', '--', '++', 'xx', 'OO', '\\|']
+    colors= ['skyblue', 'orange', 'lightgreen','tomato', 'mediumorchid', 'xkcd:medium brown', 'xkcd:pale pink', 'xkcd:greyish', 'xkcd:aquamarine', 'xkcd:dodger blue', 'xkcd:sun yellow', 'xkcd:flat green', 'xkcd:red orange', 'xkcd:lighter purple', 'xkcd:silver']
+    x = np.arange(len(labels))  # the label locations
+    width = 0.125  # the width of the bars
+    SMALL_SIZE = 8
+    MEDIUM_SIZE = 15
+    BIGGER_SIZE = 21
+    plt.rc('font', size=MEDIUM_SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=MEDIUM_SIZE)  # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=MEDIUM_SIZE)  # legend fontsize
+    plt.rc('figure', titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+    fig, ax = plt.subplots()
+    rects = {}
+    columns = list(results_map.columns)[2:]
+    for column in range(len(columns)):
+        ax.bar((x * width * 20) + (width * (column)), results_map[columns[column]], width, label=columns[column], color=colors[column], hatch=patterns[column])
 
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Frequency')
+    vals = ax.get_yticks()
+    ax.set_yticklabels(['{:,.0%}'.format(x) for x in vals])
+    ax.set_xticks(x*2.5+0.88)
+    ax.set_xticklabels(labels)
+    #ax.legend()
+
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    lgd = fig.legend(by_label.values(), by_label.keys(), loc='lower center', ncol = 5, bbox_to_anchor=(0.55, 1.0))
+    text = fig.text(-0.2, 1.05, "", transform=ax.transAxes)
+    fig.tight_layout()
+    fig.set_size_inches(10, 3)
+    fig.savefig(os.path.join(result_path, "pp_pipeline_study2.pdf"), bbox_extra_artists=(lgd,text), bbox_inches='tight')
+
+    '''
     fig, axes = plt.subplots(nrows=1, ncols=1)
     results_map.plot(ax=axes, kind='bar', x='algorithm')
     axes.get_legend().remove()
@@ -85,9 +145,9 @@ def main():
     by_label = dict(zip(labels, handles))
     lgd = fig.legend(by_label.values(), by_label.keys(), loc='lower center', ncol = 8, bbox_to_anchor=(0.5, 1.0))
     text = fig.text(-0.2, 1.05, "", transform=axes.transAxes)
-    fig.set_size_inches(20, 10, forward=True)
-    fig.tight_layout(h_pad=3.0, w_pad=4.0)
+    fig.set_size_inches(10, 5, forward=True)
+    fig.tight_layout()
     fig.savefig(os.path.join(result_path, "pp_pipeline_study2.pdf"), bbox_extra_artists=(lgd,text), bbox_inches='tight')
-
+    '''
 
 main()
